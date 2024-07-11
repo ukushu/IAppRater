9ad963e0ce3a9227e48e1de36bab5fb7577afc8f
UKS <skulptorrr@gmail.com>
2024-07-12 00:05:07 +0300

    initial

---
+++ Sources/IAppRater/IAppRater.swift
@@ -0,0 +1,141 @@
+import StoreKit
+
+@available(macOS 12, *)
+public class IAppRater {
+    public let minLaunches: Int
+    public let minDays: Int
+    
+    private let rateWndType: RateType
+    
+    private let config = UserDefaults.standard
+    
+    private let other: () -> Bool
+    
+    init(minLaunches: Int = 0, minDays: Int = 0, other: @escaping () -> Bool = { true }, rateWndType: RateType) {
+        self.minLaunches = minLaunches
+        self.minDays = minDays
+        self.rateWndType = rateWndType
+        self.other = other
+        
+        if firstLaunchDate == nil { firstLaunchDate = Date.now }
+        
+        self.launchesCount += 1
+    }
+    
+    public var isNeeded: Bool {
+        guard lastReviewVersion != appVersion,
+              other()
+        else { return false }
+        
+        return daysAfterLastReview >= 125 ||
+            ( launchesCount >= minLaunches && daysAfterFirstLaunch >= minDays )
+    }
+    
+    @discardableResult
+    public func requestIfNeeded() -> Bool {
+        guard isNeeded else { return false }
+        
+        request()
+        
+        return true
+    }
+    
+    /// Use this method only for custom rate logic, or use requestIfNeeded() instead
+    public func request() {
+        switch self.rateWndType {
+        case .standardAlert:
+            displayStandartAlert()
+        case .appStoreWnd(let appId):
+            openAppStoreRate(appId: appId)
+        }
+        
+        lastReviewDate = Date.now
+        lastReviewVersion = appVersion
+    }
+}
+
+public enum RateType {
+    case standardAlert
+    
+    ///.
+    ///
+    /// you can get app id from AppStore link:
+    /// https://apps.apple.com/ua/app/taogit/id1582642693
+    ///
+    /// here appId is: 1582642693
+    case appStoreWnd(appId: String)
+}
+
+@available(macOS 12, *)
+public extension IAppRater {
+    var launchesCount: Int {
+        get { config.integer(forKey: "ArrRaterLaunchesCounter") }
+        set(value) { config.set(value, forKey: "ArrRaterLaunchesCounter") }
+    }
+    
+    var firstLaunchDate: Date? {
+        get { config.object(forKey: "ArrRaterFirstLaunchDate") as? Date }
+        set(value) { config.set(value, forKey: "ArrRaterFirstLaunchDate") }
+    }
+    
+    var lastReviewDate: Date? {
+        get { config.object(forKey: "ArrRaterLastReviewDate") as? Date }
+        set(value) { config.set(value, forKey: "ArrRaterLastReviewDate") }
+    }
+    
+    var lastReviewVersion: String? {
+        get { config.string(forKey: "ArrRaterLastReviewVersion") }
+        set(value) { config.set(value, forKey: "ArrRaterLastReviewVersion") }
+    }
+    
+    var daysAfterFirstLaunch: Int {
+        return firstLaunchDate?.daysDistanceTo(date: Date.now) ?? 0
+    }
+    
+    var daysAfterLastReview: Int {
+        lastReviewDate?.daysDistanceTo(date: Date.now) ?? 0
+    }
+}
+
+//////////////////////
+///HELPERS
+///////////////////////
+
+internal var appVersion: String {
+    Bundle.main.object( forInfoDictionaryKey: "CFBundleShortVersionString" ) as! String
+}
+
+ 
+
+fileprivate extension Date {
+    func daysDistanceTo(date end: Date) -> Int {
+        Calendar.current.dateComponents([.day], from: self, to: end).day ?? 0
+    }
+}
+
+//////////////////////
+///Show AppReview wnd
+///////////////////////
+
+@available(macOS 12, *)
+fileprivate func displayStandartAlert() {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+#if os(iOS)
+        if #available(iOS 14.0, *) {
+            if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
+                SKStoreReviewController.requestReview(in: scene)
+            }
+        } else {
+            SKStoreReviewController.requestReview()
+        }
+#else
+        SKStoreReviewController.requestReview()
+#endif
+    }
+}
+
+fileprivate func openAppStoreRate(appId: String) {
+    if let url = URL(string: "https://apps.apple.com/app/id\(appId)?action=write-review"), !url.absoluteString.isEmpty {
+        NSWorkspace.shared.open(url)
+    }
+}
---
+++ Tests/IAppRaterTests/IAppRaterTests.swift
@@ -0,0 +1,158 @@
+import XCTest
+@testable import IAppRater
+
+final class IAppRaterTests: XCTestCase {
+    override func setUp() {
+        UserDefaults.standard.clear()
+    }
+    
+    func testMinLaunches_1() throws {
+        // launch 1
+        var review = IAppRater.prefs_3_0() // item created firstly, so launchesCount == 1
+        let tmp = review.launchesCount
+        XCTAssertEqual(review.launchesCount, 1)
+        XCTAssertNil(review.lastReviewDate)
+        XCTAssertNil(review.lastReviewVersion)
+        XCTAssertFalse(review.requestIfNeeded())
+        
+        // Launch 2
+        review = IAppRater.prefs_3_0()
+        XCTAssertFalse(review.requestIfNeeded())
+        XCTAssertEqual(review.isNeeded, false)
+        
+        // Launch 3, need to show review window
+        review = IAppRater.prefs_3_0()
+        XCTAssertEqual(review.isNeeded, true)
+        XCTAssertTrue(review.requestIfNeeded())
+        XCTAssertEqual(review.launchesCount, 3)
+        XCTAssertNotNil(review.lastReviewDate)
+        XCTAssertNotNil(review.lastReviewVersion)
+        XCTAssertEqual(review.isNeeded, false)
+        
+        // Launch 4
+        review = IAppRater.prefs_3_0()
+        XCTAssertEqual(review.isNeeded, false)
+        XCTAssertFalse(review.requestIfNeeded())
+        XCTAssertEqual(review.launchesCount, 4)
+    }
+    
+    func testMinLaunches_2() throws {
+        //1 = static init
+        //2 and next = reinit125_0
+        var review: IAppRater!
+        
+        for i in (1...124) {
+            review = IAppRater.prefs_125_0()
+            XCTAssertEqual(review.isNeeded, false)
+            XCTAssertEqual(review.launchesCount, i)
+            XCTAssertFalse(review.requestIfNeeded())
+        }
+        
+        //Day 125
+        review = IAppRater.prefs_125_0()
+        XCTAssertEqual(review.isNeeded, true)
+        XCTAssertEqual(review.launchesCount, 125)
+    }
+    
+    func testMinDays() throws {
+        var review = IAppRater.prefs_3_0()
+        // item created firstly, so launchesCount == 1
+        XCTAssertEqual(review.launchesCount, 1)
+        XCTAssertNotNil(review.firstLaunchDate)
+        XCTAssertNil(review.lastReviewDate)
+        XCTAssertNil(review.lastReviewVersion)
+        
+        review = IAppRater.prefs_3_125()
+        
+        // Launch 2
+        XCTAssertFalse(review.requestIfNeeded())
+        XCTAssertEqual(review.launchesCount, 2)
+        XCTAssertNil(review.lastReviewDate)
+        XCTAssertNil(review.lastReviewVersion)
+        XCTAssertEqual(review.daysAfterFirstLaunch, 0)
+        
+        // Launch 3 in 4 days after first launch
+        review = IAppRater.prefs_3_125()
+        review.firstLaunchDate = Date.now.addingTimeInterval(TimeInterval(days: -4))
+        XCTAssertEqual(review.launchesCount, 3)
+        XCTAssertEqual(review.daysAfterFirstLaunch, 4)
+        XCTAssertFalse(review.requestIfNeeded()) // we expecte request on 3d launch
+        
+        // Launch 4
+        review = IAppRater.prefs_3_125()
+        XCTAssertFalse(review.requestIfNeeded())
+        XCTAssertEqual(review.launchesCount, 4)
+        
+        // Launch 4 in 124 days after first launch
+        review.firstLaunchDate = Date.now.addingTimeInterval(TimeInterval(days: -124))
+        XCTAssertEqual(review.launchesCount, 4)
+        XCTAssertEqual(review.daysAfterFirstLaunch, 124)
+        XCTAssertNil(review.lastReviewDate)
+        XCTAssertNil(review.lastReviewVersion)
+        XCTAssertEqual(review.isNeeded, false)
+        XCTAssertFalse(review.requestIfNeeded())
+        
+        // Launch 4 in 125 days after last review
+        review.lastReviewDate = Date.now.addingTimeInterval(TimeInterval(days:-125))
+        XCTAssertEqual(review.daysAfterLastReview, 125)
+        XCTAssertEqual(review.isNeeded, true)
+        XCTAssertTrue(review.requestIfNeeded())
+        XCTAssertEqual(review.isNeeded, false)
+        XCTAssertEqual(review.launchesCount, 4)
+        XCTAssertFalse(review.requestIfNeeded())
+        
+        // app version is the same, so no need to show
+        review.lastReviewVersion = appVersion
+        review.lastReviewDate = Date.now.addingTimeInterval(TimeInterval(days:-125))
+        XCTAssertEqual(review.isNeeded, false)
+        XCTAssertFalse(review.requestIfNeeded())
+        XCTAssertEqual(review.launchesCount, 4)
+        
+        // app version changed, so need to show and lastReviewDate > 125 so need to show
+        review.lastReviewVersion = "125.0.1"
+        review.lastReviewDate = Date.now.addingTimeInterval(TimeInterval(days:-125))
+        let lastReviewDate = review.lastReviewDate
+        XCTAssertTrue(review.requestIfNeeded())
+        XCTAssertEqual(review.launchesCount, 4)
+        XCTAssertNotEqual(review.lastReviewDate, lastReviewDate)
+    }
+    
+    func testCustomRules() throws {
+        var review = IAppRater(minLaunches: 0, minDays: 0, other: { true }, rateWndType: .standardAlert)
+        
+        XCTAssertEqual(review.isNeeded, true)
+        
+        review = IAppRater(minLaunches: 0, minDays: 0, other: { false }, rateWndType: .standardAlert)
+        
+        XCTAssertEqual(review.isNeeded, false)
+    }
+}
+
+fileprivate extension UserDefaults {
+    func clear() {
+        for key in dictionaryRepresentation().keys {
+            removeObject(forKey: key)
+        }
+    }
+}
+
+fileprivate extension IAppRater {
+    static func prefs_3_0() -> IAppRater {
+        IAppRater(minLaunches: 3, minDays: 0, rateWndType: .standardAlert)
+    }
+    
+    static func prefs_3_125() -> IAppRater {
+        IAppRater(minLaunches: 3, minDays: 125, rateWndType: .standardAlert)
+    }
+    
+    static func prefs_125_0() -> IAppRater {
+        IAppRater(minLaunches: 125, minDays: 0, rateWndType: .standardAlert)
+    }
+}
+
+fileprivate extension TimeInterval {
+    init(days: Int = 0, hrs: Int = 0, mins: Int = 0, sec: Int = 0) {
+        let timeInSec = (sec) + (mins * 60) + (hrs * 60 * 60) + (days * 24 * 60 * 60)
+        self.init(timeInSec)
+    }
+}
